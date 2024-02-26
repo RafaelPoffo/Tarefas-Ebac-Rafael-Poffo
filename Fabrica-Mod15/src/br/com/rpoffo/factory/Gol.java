2c2137c58972572c8b27613d09ff27dd033e3a7b
Rafael Poffo Filho <133990943+RafaelPoffo@users.noreply.github.com>
2024-02-26 14:12:45 -0300

    Delete Fabrica-Mod15 directory

--- Fabrica-Mod15/src/br/com/rpoffo/factory/Gol.java
+++
@@ -1,7 +0,0 @@
-package br.com.rpoffo.factory;
-
-public class Gol extends Car {
-    public Gol(int horsePower, java.lang.String fuelSource, java.lang.String color) {
-        super(horsePower, fuelSource, color);
-    }
-}
--- Fabrica-Mod15/src/br/com/rpoffo/factory/Palio.java
+++
@@ -1,9 +0,0 @@
-package br.com.rpoffo.factory;
-
-public class Palio extends Car{
-
-
-    public Palio(int horsePower, java.lang.String fuelSource, java.lang.String color) {
-        super(horsePower, fuelSource, color);
-    }
-}

51bc5b7d8c29c8588bb0f1f3e8cce13b0c45fa76
Rafael Poffo Filho <133990943+RafaelPoffo@users.noreply.github.com>
2024-03-22 18:14:12 -0300

    Update IClienteDAO.java

--- ExemplosJDBCAlunos/src/java/main/br/com/rpoffo/dao/IClienteDAO.java
+++ ExemplosJDBCAlunos/src/java/main/br/com/rpoffo/dao/IClienteDAO.java
@@ -5,9 +5,10 @@ package br.com.rpoffo.dao;
 
 import br.com.rpoffo.domain.Cliente;
 
+/**
+ * 
+ */
 public interface IClienteDAO {
-
-	
 	public Integer cadastrar(Cliente cliente) throws Exception;
 
 	public Cliente consultar(String codigo) throws Exception;

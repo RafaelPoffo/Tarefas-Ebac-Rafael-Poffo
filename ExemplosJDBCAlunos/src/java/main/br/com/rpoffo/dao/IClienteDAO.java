254a4ac8bc81a94ed739fd1d696b28917fee562d
Rafael Poffo Filho <133990943+RafaelPoffo@users.noreply.github.com>
2024-03-25 11:33:40 -0300

    Add files via upload

---
+++ ExemplosJDBCAlunos/src/java/main/br/com/rpoffo/dao/IClienteDAO.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package br.com.rpoffo.dao;
+
+import br.com.rpoffo.domain.Cliente;
+
+
+public interface IClienteDAO {
+
+	
+	public Integer cadastrar(Cliente cliente) throws Exception;
+
+	public Cliente consultar(String codigo) throws Exception;
+
+	public Integer excluir(Cliente clienteBD) throws Exception;
+}

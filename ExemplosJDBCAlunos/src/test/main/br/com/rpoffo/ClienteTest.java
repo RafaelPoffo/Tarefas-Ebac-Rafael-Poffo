99e7b47dd6af60880436c4e2830acee6abcefc71
Rafael Poffo Filho <133990943+RafaelPoffo@users.noreply.github.com>
2024-03-25 11:27:37 -0300

    Add files via upload

---
+++ ExemplosJDBCAlunos/src/test/main/br/com/rpoffo/ClienteTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package br.com.rpoffo;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import br.com.rpoffo.dao.ClienteDAO;
+import br.com.rpoffo.dao.IClienteDAO;
+import br.com.rpoffo.domain.Cliente;
+
+/**
+ * 
+ */
+public class ClienteTest {
+	@Test
+	public void cadastrarTest() throws Exception {
+		IClienteDAO dao = new ClienteDAO();
+		
+		Cliente cliente = new Cliente();
+		cliente.setCodigo("01");
+		cliente.setNome("Rafael Poffo");
+		
+		Integer qtd = dao.cadastrar(cliente);
+		assertTrue(qtd == 1);
+		
+		Cliente clienteBD = dao.consultar(cliente.getCodigo());
+		assertNotNull(clienteBD);
+		assertNotNull(clienteBD.getId());
+		assertEquals(cliente.getCodigo(), clienteBD.getCodigo());
+		assertEquals(cliente.getNome(), clienteBD.getNome());
+		
+		Integer qtdDel = dao.excluir(clienteBD);
+		assertNotNull(qtdDel);
+	}
+}

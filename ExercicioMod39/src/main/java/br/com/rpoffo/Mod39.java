d2a8872e7d1c63888c7b429f5568ec709c432805
Rafael Poffo Filho <133990943+RafaelPoffo@users.noreply.github.com>
2024-04-17 17:03:10 -0300

    Commit mod39

---
+++ ExercicioMod39/src/main/java/br/com/rpoffo/Mod39.java
@@ -0,0 +1,40 @@
+package br.com.rpoffo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class App38
+ */
+@WebServlet("/Mod39")
+public class Mod39 extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Mod39() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().append("Exercicio Mod39 EBAC");
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
+    }
+
+}

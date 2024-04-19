dff1635ae8f4968dcfd8cbd94ac61121752fd266
Rafael Poffo Filho <133990943+RafaelPoffo@users.noreply.github.com>
2024-04-19 12:12:04 -0300

    Commit mod42

---
+++ Mod42/ConfigService/src/main/java/br/com/rpoffo/vendas/online/ConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.rpoffo.vendas.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+
+}
